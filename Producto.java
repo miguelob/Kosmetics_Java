09dd672974865ec5dc0850cd6f7ef7fabd1b3176
Miguel <46598884+miguelob@users.noreply.github.com>
2019-09-23 16:45:13 +0200

    First version of product obj

---
+++ Producto.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+
+public abstract class Producto{
+    public final int idProduct;
+    private String name;
+    private double price;
+    private String brand;
+    private String description;
+    private HashMap<String,Integer> survey;
+
+    public Producto(int idProduct, String name, double price, String brand, String description){
+        this.idProduct = idProduct;
+        this.name = name;
+        this.price = price;
+        this.brand = brand;
+        this.description = description;
+    }
+    public abstract setHashMap();
+
+    public String getName(){
+        return name;
+    }
+    public double getPrice(){
+        return price;
+    }
+    public String getBrand(){
+        return brand;
+    }
+    public String getDescription(){
+        return description;
+    }
+
+}

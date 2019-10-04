937c8589628e6cad4d1c1558748bbdd5d0725d21
maroacc <45521548+maroacc@users.noreply.github.com>
2019-10-04 21:26:26 +0200

    poh favoh no tocar

---
+++ projectISW19/src/icai/dtc/isw/gui/GUIConstants.java
@@ -0,0 +1,19 @@
+package icai.dtc.isw.gui;
+import java.awt.Font;
+import java.awt.Color;
+
+public class GUIConstants {
+	
+	  public static final Font FONT_REGULAR = new Font("Font.SERIF_MD", Font.PLAIN, 16);
+	  public static final Font FONT_REGULAR_BOLD = new Font("Font.SERIF_MD", Font.BOLD, 16);
+	  /**Fuente para títulos de la interfaz gráfica, en este caso "SERIF_MD", en negrita y de tamano 20
+	  */
+	  public static final Font FONT_TITLE = new Font("Font.SERIF_MD", Font.BOLD, 20);
+	  /**Fuente para títulos principales de la interfaz gráfica, en este caso "SERIF_MD", en negrita y de tamano 20
+	  */
+	  public static final Font FONT_MEDIUM_TITLE = new Font("Font.SERIF_MD", Font.BOLD, 40);
+	  public static final Font FONT_BIG_TITLE = new Font("Font.SERIF_MD", Font.BOLD, 60);
+	  
+	  public static final Color DEFAULT_FONT_COLOR = new Color(80, 80, 80);
+
+}
---
+++ projectISW19/src/icai/dtc/isw/gui/MyJLabel.java
@@ -0,0 +1,42 @@
+package icai.dtc.isw.gui;
+import javax.swing.BorderFactory;
+import javax.swing.Icon;
+import javax.swing.JLabel;
+
+public class MyJLabel extends JLabel {
+
+	public MyJLabel() {
+		this.changeBackgroundColor();
+	}
+
+	public MyJLabel(String text) {
+		super(text);
+		this.changeBackgroundColor();
+	}
+
+	public MyJLabel(Icon image) {
+		super(image);
+		this.changeBackgroundColor();
+	}
+
+	public MyJLabel(String text, int horizontalAlignment) {
+		super(text, horizontalAlignment);
+		this.changeBackgroundColor();
+	}
+
+	public MyJLabel(Icon image, int horizontalAlignment) {
+		super(image, horizontalAlignment);
+		this.changeBackgroundColor();
+	}
+
+	public MyJLabel(String text, Icon icon, int horizontalAlignment) {
+		super(text, icon, horizontalAlignment);
+		this.changeBackgroundColor();
+	}
+	
+	public void changeBackgroundColor(){
+		this.setBorder(BorderFactory.createEmptyBorder());
+		this.setFont(GUIConstants.FONT_REGULAR_BOLD);
+	}
+
+}

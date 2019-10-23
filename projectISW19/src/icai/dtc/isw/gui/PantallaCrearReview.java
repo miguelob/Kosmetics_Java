119a59a1157b69e6032ba2848620ae6bb720a5d5
maroacc <45521548+maroacc@users.noreply.github.com>
2019-10-23 11:27:53 +0200

    Prueba Branch
    Co-Authored-By: Miguel <miguelob@users.noreply.github.com>

---
+++ projectISW19/src/icai/dtc/isw/gui/PantallaCrearReview.java
@@ -0,0 +1,254 @@
+package icai.dtc.isw.gui;
+
+import java.awt.HeadlessException;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.EventQueue;
+
+import javax.swing.SwingConstants;
+import javax.swing.border.MatteBorder;
+import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
+import javax.swing.JTextPane;
+import java.awt.GridLayout;
+import javax.swing.border.LineBorder;
+import java.awt.Font;
+import javax.swing.JRadioButton;
+
+public class PantallaCrearReview extends JFrame {
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					PantallaCrearReview frame = new PantallaCrearReview();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	public PantallaCrearReview() throws HeadlessException {
+		
+		//Header
+		//Brand name to the left
+		//Profile button to the right
+		
+		JPanel header = new JPanel();
+		header.setForeground(new Color(0, 0, 0));
+		header.setBorder(new MatteBorder(15, 10, 15, 15, (Color) new Color(255, 153, 153)));
+		header.setBackground(new Color(255, 153, 153));
+		getContentPane().add(header, BorderLayout.NORTH);
+		header.setLayout(new BorderLayout(0, 0));
+		
+		//Brand name
+		//Button that takes the user to the main page
+		MyJButton mjbtnKosmetics = new MyJButton("Kosmetics.");
+		header.add(mjbtnKosmetics, BorderLayout.WEST);
+		
+		//Profile button
+		//Takes the user to his profile
+		MyJButton mjbtnProfile = new MyJButton("Profile");
+		header.add(mjbtnProfile, BorderLayout.EAST);
+		
+		//Main panel
+		//Includes product name
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(Color.WHITE);
+		getContentPane().add(panel_1, BorderLayout.CENTER);
+		panel_1.setLayout(new BorderLayout(0, 0));
+		
+		//Products name
+		
+		MyJLabel mjlblProductName = new MyJLabel("Lip Blush Bundle");
+		mjlblProductName.setHorizontalAlignment(SwingConstants.CENTER);
+		mjlblProductName.setFont(GUIConstants.FONT_BIG_TITLE);
+		panel_1.add(mjlblProductName, BorderLayout.NORTH);
+		
+		//Sub panel to the main panel
+		//Does not include the products name
+		JPanel panel_2 = new JPanel();
+		panel_2.setBorder(new MatteBorder(50, 200, 50, 100, (Color) new Color(255, 255, 255)));
+		panel_2.setBackground(Color.WHITE);
+		panel_1.add(panel_2, BorderLayout.CENTER);
+		panel_2.setLayout(new BorderLayout(0, 0));
+		
+		//Panel with buttons for the stars
+		//The user can choose how many stars will be given to the product
+		
+		JPanel panelStars = new JPanel();
+		panelStars.setLayout(new BorderLayout());
+		panelStars.setBackground(new Color(255, 255, 255));
+		panel_2.add(panelStars, BorderLayout.NORTH);
+
+		JPanel panelStarsDescription = new JPanel();
+		panelStarsDescription.setBackground(Color.WHITE);
+		panelStarsDescription.add(new MyJLabel("Your overall rating of this product"));
+		panelStars.add(panelStarsDescription, BorderLayout.NORTH);
+		JPanel panelStarFlow = new JPanel();
+		panelStarFlow.setBackground(Color.WHITE);
+		panelStars.add(panelStarFlow, BorderLayout.CENTER);
+		
+		MyJButton2States lblStar_1 = new MyJButton2States(new ImageIcon("media/icons/star.png"), new ImageIcon("media/icons/star.png"), new ImageIcon("media/icons/star.png"), new ImageIcon("media/icons/star.png"));
+		lblStar_1.setContentAreaFilled(false);
+		panelStarFlow.add(lblStar_1);
+		MyJButton2States lblStar_2 = new MyJButton2States(new ImageIcon("media/icons/star.png"), new ImageIcon("media/icons/star.png"), new ImageIcon("media/icons/star.png"), new ImageIcon("media/icons/star.png"));
+		panelStarFlow.add(lblStar_2);
+		MyJButton2States lblStar_3 = new MyJButton2States(new ImageIcon("media/icons/star.png"), new ImageIcon("media/icons/star.png"), new ImageIcon("media/icons/star.png"), new ImageIcon("media/icons/star.png"));
+		panelStarFlow.add(lblStar_3);
+		MyJButton2States lblStar_4 = new MyJButton2States(new ImageIcon("media/icons/star.png"), new ImageIcon("media/icons/star.png"), new ImageIcon("media/icons/star.png"), new ImageIcon("media/icons/star.png"));
+		panelStarFlow.add(lblStar_4);
+		MyJButton2States lblStar_5 = new MyJButton2States(new ImageIcon("media/icons/star.png"), new ImageIcon("media/icons/star.png"), new ImageIcon("media/icons/star.png"), new ImageIcon("media/icons/star.png"));
+		panelStarFlow.add(lblStar_5);
+		
+		//Panel for the review's text and quick survey
+		JPanel panelShareYourExperience = new JPanel();
+		panelShareYourExperience.setLayout(new BorderLayout());
+		panel_2.add(panelShareYourExperience, BorderLayout.CENTER);
+		JTextPane txtpnShareYourExperience = new JTextPane();
+		txtpnShareYourExperience.setFont(new Font("SansSerif", Font.PLAIN, 15));
+		txtpnShareYourExperience.setCaretColor(Color.BLACK);
+		txtpnShareYourExperience.setBorder(new LineBorder(new Color(192, 192, 192), 2));
+		txtpnShareYourExperience.setText("Share your experience: the quality, the price, did it meet your expectations?...");
+		txtpnShareYourExperience.setForeground(Color.DARK_GRAY);
+		panelShareYourExperience.add(txtpnShareYourExperience, BorderLayout.CENTER);
+		
+		//Panel for the quick survey
+		//It includes the product's questions
+		//and 3 possible answers : "yes", "I'm not sure", "no".
+		JPanel panelSurvey = new JPanel();
+		panelSurvey.setBorder(new MatteBorder(25, 1, 1, 1, (Color) Color.WHITE));
+		panelSurvey.setBackground(Color.WHITE);
+		panelShareYourExperience.add(panelSurvey, BorderLayout.SOUTH);
+		panelSurvey.setLayout(new GridLayout(0, 1, 0, 0));
+		addSurvey(panelSurvey);
+
+		
+		
+		//Panel with other users' reviews 
+		JPanel panelAside = new JPanel();
+		panelAside.setBorder(new MatteBorder(1, 100, 1, 50, (Color) new Color(255, 255, 255)));
+		panelAside.setBackground(Color.WHITE);
+		panel_2.add(panelAside, BorderLayout.EAST);
+		panelAside.setLayout(new GridLayout(0, 1, 0, 0));
+		
+		//Panel for an indivifual review from a different user
+		JPanel panelIndividualReview = new JPanel();
+		panelIndividualReview.setBackground(Color.WHITE);
+		panelAside.add(panelIndividualReview);
+		panelIndividualReview.setLayout(new BorderLayout(0, 0));
+		
+		MyJLabel mjlblUsername = new MyJLabel("maroacc");
+		mjlblUsername.setFont(GUIConstants.FONT_REGULAR);
+		panelIndividualReview.add(mjlblUsername, BorderLayout.NORTH);
+		
+		MyJLabel mjlblReviewText = new MyJLabel("<html>The lip blush formula in entirely different than the matte lip kits, <br>"
+				+ "but still has the vibrant beautiful appearance. <br>"
+				+ "It’s light weight and smooth, feels like butter. This product doesn’t get crusty or dry out. <br>"
+				+ "All four shades are stunning and highly pigmented. New favorite for sure!!</html>");
+		mjlblReviewText.setFont(GUIConstants.FONT_REGULAR);
+		mjlblReviewText.setForeground(Color.LIGHT_GRAY);
+		panelIndividualReview.add(mjlblReviewText, BorderLayout.CENTER);
+		
+		MyJLabel mjlblUser = new MyJLabel("maroacc");
+		panelAside.add(mjlblUser);
+	}
+	
+	public void addSurvey(JPanel panelSurvey)
+	{
+		//First Question
+		MyJLabel mjlblIsItLong = new MyJLabel("Is it long-lasting?");
+		panelSurvey.add(mjlblIsItLong);
+		
+		JPanel panelSurveyRadioButtons = new JPanel();
+		panelSurveyRadioButtons.setBackground(Color.WHITE);
+		panelSurvey.add(panelSurveyRadioButtons);
+		
+		JRadioButton rdbtnYes = new JRadioButton("Yes");
+		rdbtnYes.setBackground(Color.WHITE);
+		rdbtnYes.setFont(GUIConstants.FONT_REGULAR);
+		panelSurveyRadioButtons.add(rdbtnYes);
+		
+		JRadioButton rdbtnNotSure = new JRadioButton("I'm not sure");
+		rdbtnNotSure.setBackground(Color.WHITE);
+		rdbtnNotSure.setFont(GUIConstants.FONT_REGULAR);
+		panelSurveyRadioButtons.add(rdbtnNotSure);
+		
+		JRadioButton rdbtnNo = new JRadioButton("No");
+		rdbtnNo.setBackground(Color.WHITE);
+		rdbtnNo.setFont(GUIConstants.FONT_REGULAR);
+		panelSurveyRadioButtons.add(rdbtnNo);
+		
+		ButtonGroup group = new ButtonGroup();
+		group.add(rdbtnYes);
+		group.add(rdbtnNotSure);
+		group.add(rdbtnNo);
+
+		
+		//Second Question
+		
+		MyJLabel mjlblHighPigmented = new MyJLabel("Is it highly pigmented?");
+		panelSurvey.add(mjlblHighPigmented);
+		
+		JPanel panelSurveyRadioButtons2 = new JPanel();
+		panelSurveyRadioButtons2.setBackground(Color.WHITE);
+		panelSurvey.add(panelSurveyRadioButtons2);
+		
+		JRadioButton rdbtnYes2 = new JRadioButton("Yes");
+		rdbtnYes2.setBackground(Color.WHITE);
+		rdbtnYes2.setFont(GUIConstants.FONT_REGULAR);
+		panelSurveyRadioButtons2.add(rdbtnYes2);
+		
+		JRadioButton rdbtnNotSure2 = new JRadioButton("I'm not sure");
+		rdbtnNotSure2.setBackground(Color.WHITE);
+		rdbtnNotSure2.setFont(GUIConstants.FONT_REGULAR);
+		panelSurveyRadioButtons2.add(rdbtnNotSure2);
+		
+		JRadioButton rdbtnNo2 = new JRadioButton("No");
+		rdbtnNo2.setBackground(Color.WHITE);
+		rdbtnNo2.setFont(GUIConstants.FONT_REGULAR);
+		panelSurveyRadioButtons2.add(rdbtnNo2);
+		
+		ButtonGroup group2 = new ButtonGroup();
+		group2.add(rdbtnYes2);
+		group2.add(rdbtnNotSure2);
+		group2.add(rdbtnNo2);
+		
+		//Third Question
+		
+		MyJLabel mjlblMatte = new MyJLabel("Is it matte?");
+		panelSurvey.add(mjlblMatte);
+		
+		JPanel panelSurveyRadioButtons3 = new JPanel();
+		panelSurveyRadioButtons3.setBackground(Color.WHITE);
+		panelSurvey.add(panelSurveyRadioButtons3);
+		
+		JRadioButton rdbtnYes3 = new JRadioButton("Yes");
+		rdbtnYes3.setBackground(Color.WHITE);
+		rdbtnYes3.setFont(GUIConstants.FONT_REGULAR);
+		panelSurveyRadioButtons3.add(rdbtnYes3);
+		
+		JRadioButton rdbtnNotSure3 = new JRadioButton("I'm not sure");
+		rdbtnNotSure3.setBackground(Color.WHITE);
+		rdbtnNotSure3.setFont(GUIConstants.FONT_REGULAR);
+		panelSurveyRadioButtons3.add(rdbtnNotSure3);
+		
+		JRadioButton rdbtnNo3 = new JRadioButton("No");
+		rdbtnNo3.setBackground(Color.WHITE);
+		rdbtnNo3.setFont(GUIConstants.FONT_REGULAR);
+		panelSurveyRadioButtons3.add(rdbtnNo3);
+		
+		ButtonGroup group3 = new ButtonGroup();
+		group3.add(rdbtnYes3);
+		group3.add(rdbtnNotSure3);
+		group3.add(rdbtnNo3);
+		
+	}
+
+
+}
---
+++ projectISW19/src/icai/dtc/isw/gui/PantallaProductos.java
@@ -0,0 +1,330 @@
+package icai.dtc.isw.gui;
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.Toolkit;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import javax.swing.ImageIcon;
+import java.awt.Dimension;
+
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Panel;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import java.awt.Color;
+import javax.swing.JCheckBox;
+import java.awt.GridLayout;
+import javax.swing.JSpinner;
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import java.awt.FlowLayout;
+import java.awt.Checkbox;
+import java.awt.Label;
+import javax.swing.JSlider;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.SwingConstants;
+import javax.swing.JFormattedTextField;
+import javax.swing.JTextPane;
+import javax.swing.JRadioButton;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
+import javax.swing.JList;
+import javax.swing.border.LineBorder;
+import javax.swing.border.MatteBorder;
+
+import icai.dtc.isw.client.Client;
+import icai.dtc.isw.domain.Product;
+
+
+public class PantallaProductos extends JFrame {
+	private JFormattedTextField txtWhatAreYou;
+    ArrayList products;
+    Client client;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					PantallaProductos frame = new PantallaProductos();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public PantallaProductos() {
+		BorderLayout borderLayout = (BorderLayout) getContentPane().getLayout();
+		borderLayout.setVgap(20);
+		borderLayout.setHgap(25);
+//      EN TEORIA PONE EN PANTALLA COMPLETA PERO NO SE AJUSTA
+		Toolkit tk = Toolkit.getDefaultToolkit();
+		int xSize = ((int) tk.getScreenSize().getWidth());
+		int ySize = ((int) tk.getScreenSize().getHeight());
+		//this.setSize(617,479);
+		
+		
+		setBackground(Color.WHITE);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 797, 480);
+		getContentPane().setBackground(new Color(255, 204, 204));
+		
+		MyJPanel myJPanel = new MyJPanel();
+		myJPanel.setBorder(new MatteBorder(15, 10, 15, 15, (Color) new Color(255, 153, 153)));
+		myJPanel.setBackground(new Color(255, 153, 153));
+		getContentPane().add(myJPanel, BorderLayout.NORTH);
+		myJPanel.setLayout(new BorderLayout(1, 1));
+		
+		MyJButton mjbtnProfile = new MyJButton();
+		mjbtnProfile.setBackground(new Color(255, 204, 255));
+		mjbtnProfile.setText("Profile");
+		mjbtnProfile.setIcon( new ImageIcon("media/icons/user.png"));
+		myJPanel.add(mjbtnProfile, BorderLayout.EAST);
+		
+		MyJPanel myJPanel_3 = new MyJPanel();
+		myJPanel_3.setLayout(new FlowLayout(FlowLayout.CENTER, 50, 0));
+		myJPanel_3.setBackground(new Color(255, 153, 153));
+		myJPanel.add(myJPanel_3, BorderLayout.SOUTH);
+		
+		MyJButton mjbtnFoundation = new MyJButton(); 
+		mjbtnFoundation.setText("FOUNDATION");
+		myJPanel_3.add(mjbtnFoundation);
+		
+		MyJButton mjbtnMascara = new MyJButton();
+		mjbtnMascara.setText("MASCARA");
+		myJPanel_3.add(mjbtnMascara);
+		
+		MyJButton mjbtnEyeshadow = new MyJButton();
+		mjbtnEyeshadow.setText("EYESHADOW");
+		myJPanel_3.add(mjbtnEyeshadow);
+		
+		MyJButton mjbtnLipstick = new MyJButton();
+		mjbtnLipstick.setText("LIPSTICK");
+		myJPanel_3.add(mjbtnLipstick);
+		
+		MyJLabel mjlblKosmetics = new MyJLabel();
+		myJPanel.add(mjlblKosmetics, BorderLayout.WEST);
+		mjlblKosmetics.setText("Kosmetics.");
+		
+		MyJPanel myJPanel_8 = new MyJPanel();
+		myJPanel_8.setBackground(new Color(255, 153, 153));
+		myJPanel.add(myJPanel_8, BorderLayout.CENTER);
+		
+		MyJLabel myJLabel_1 = new MyJLabel(new ImageIcon("media/icons/magnifying-glass.png"));
+		myJPanel_8.add(myJLabel_1);
+		
+		txtWhatAreYou = new JFormattedTextField();
+		txtWhatAreYou.setCaretColor(Color.BLACK);
+		txtWhatAreYou.setForeground(Color.GRAY);
+		txtWhatAreYou.setText("What are you looking for?");
+		txtWhatAreYou.setBorder(BorderFactory.createEmptyBorder());
+		txtWhatAreYou.setFont(GUIConstants.FONT_REGULAR);
+		txtWhatAreYou.addMouseListener(new MouseAdapter()
+        { @Override
+	           public void mouseClicked(MouseEvent me)
+	             { txtWhatAreYou.setText("");
+
+	             }
+	         });
+		
+		myJPanel_8.add(txtWhatAreYou);
+		
+		MyJPanel myJPanel_1 = new MyJPanel();
+		myJPanel_1.setBorder(new LineBorder(new Color(250, 180, 180), 50));
+		myJPanel_1.setBackground(new Color(250, 180, 180));
+		getContentPane().add(myJPanel_1, BorderLayout.WEST);
+		myJPanel_1.setLayout(new GridLayout(0, 1, 0, 0));
+		myJPanel_1.setSize(new Dimension(100, 100));
+		
+		MyJLabel mjlblPrice = new MyJLabel();
+		mjlblPrice.setText("Price");
+		myJPanel_1.add(mjlblPrice);
+		
+		JSlider slider = new JSlider();
+		slider.setForeground(Color.LIGHT_GRAY);
+		slider.setValue(25);
+		slider.setPaintLabels(true);
+		slider.setBackground(new Color(250, 180, 180));
+		myJPanel_1.add(slider);
+		
+		MyJLabel mjlblBrand = new MyJLabel();
+		mjlblBrand.setText("Brand");
+		myJPanel_1.add(mjlblBrand);
+		
+		Checkbox checkbox_2 = new Checkbox(" Fenty Beauty");
+		checkbox_2.setForeground(Color.WHITE);
+		checkbox_2.setFont(GUIConstants.FONT_REGULAR);
+		myJPanel_1.add(checkbox_2);
+		
+		
+		Checkbox checkbox_3 = new Checkbox(" Kylie Cosmetics");
+		checkbox_3.setForeground(Color.WHITE);
+		checkbox_3.setFont(GUIConstants.FONT_REGULAR);
+		myJPanel_1.add(checkbox_3);
+		
+		Checkbox checkbox_4 = new Checkbox(" Anastasia");
+		checkbox_4.setForeground(Color.WHITE);
+		checkbox_4.setFont(GUIConstants.FONT_REGULAR);
+		myJPanel_1.add(checkbox_4);
+		
+		Checkbox checkbox_5 = new Checkbox(" Too Faced");
+		checkbox_5.setForeground(Color.WHITE);
+		checkbox_5.setFont(GUIConstants.FONT_REGULAR);
+		myJPanel_1.add(checkbox_5);
+		
+		Checkbox checkbox = new Checkbox(" KKW Beauty");
+		checkbox.setForeground(Color.WHITE);
+		checkbox.setFont(GUIConstants.FONT_REGULAR);
+		myJPanel_1.add(checkbox);
+		
+		Checkbox checkbox_6 = new Checkbox(" Benefit");
+		checkbox_6.setForeground(Color.WHITE);
+		checkbox_6.setFont(GUIConstants.FONT_REGULAR);
+		myJPanel_1.add(checkbox_6);
+		
+		MyJLabel mjlblBrand_1 = new MyJLabel();
+		mjlblBrand_1.setText("Popularity");
+		myJPanel_1.add(mjlblBrand_1);
+		
+		Checkbox checkbox_7 = new Checkbox(" 5 stars");
+		checkbox_7.setForeground(Color.WHITE);
+		checkbox_7.setFont(GUIConstants.FONT_REGULAR);
+		myJPanel_1.add(checkbox_7);
+		
+		Checkbox checkbox_8 = new Checkbox(" 4 stars");
+		checkbox_8.setForeground(Color.WHITE);
+		checkbox_8.setFont(GUIConstants.FONT_REGULAR);
+		myJPanel_1.add(checkbox_8);
+		
+		Checkbox checkbox_11 = new Checkbox(" 3 stars");
+		checkbox_11.setForeground(Color.WHITE);
+		checkbox_11.setFont(GUIConstants.FONT_REGULAR);
+		myJPanel_1.add(checkbox_11);
+		
+		Checkbox checkbox_9 = new Checkbox(" 2 stars");
+		checkbox_9.setForeground(Color.WHITE);
+		checkbox_9.setFont(GUIConstants.FONT_REGULAR);
+		myJPanel_1.add(checkbox_9);
+		
+		Checkbox checkbox_10 = new Checkbox(" 1 star");
+		checkbox_10.setForeground(Color.WHITE);
+		checkbox_10.setFont(GUIConstants.FONT_REGULAR);
+		myJPanel_1.add(checkbox_10);
+		
+		//Panel for the list of products in out database
+		MyJPanel myJPanel_2 = new MyJPanel();
+		JScrollPane jsp = new JScrollPane(myJPanel_2);
+		jsp.setBorder(BorderFactory.createEmptyBorder());
+		myJPanel_2.setBorder(new MatteBorder(1, 1, 1, 100, (Color) new Color(255, 204, 204)));
+		myJPanel_2.setBackground(new Color(255, 204, 204));
+		getContentPane().add(jsp, BorderLayout.CENTER);
+		myJPanel_2.setLayout(new GridLayout(0, 1, 5, 10));
+
+		
+		initialiseProducts();
+		
+	      Iterator it = products.iterator();
+	      while (it.hasNext())
+	      {	 //Panel for every product available. Includes name, brand, category, description, price
+	          MyJPanel productPanel = new MyJPanel();
+	          productPanel.setLayout(new GridLayout(1, 0));
+	          Product product = (Product) it.next();
+	          
+	          //Button with the photo of the product
+	          //Takes you to the product's screen
+	          MyJButton mjb = new MyJButton(new ImageIcon("media/images/product0.jpg"));
+	          productPanel.add(mjb);
+	          
+	          //Panel for the name, brand, category and type
+	          MyJPanel namePanel = new MyJPanel();
+	          namePanel.setLayout(new GridLayout(0, 1));
+	          //Name of the product
+	          MyJLabel lblName = new MyJLabel(product.getName());
+	          lblName.setFont(GUIConstants.FONT_MEDIUM_TITLE);
+	          namePanel.add(lblName);
+	          //Brand of the product
+	          MyJLabel lblBrand = new MyJLabel(product.getBrand());
+	          lblBrand.setFont(GUIConstants.FONT_TITLE);
+	          namePanel.add(lblBrand);
+	          mjb.setHorizontalAlignment(SwingConstants.CENTER);
+	          //Category of the product
+	          namePanel.add(new MyJLabel(product.getCategory()));
+	          //Price of the product
+	          MyJLabel lblPrice = new MyJLabel(String.valueOf(product.getPrice()) + " $");
+	          lblPrice.setFont(GUIConstants.FONT_MEDIUM_TITLE);
+	          lblPrice.setForeground(new Color(255, 113, 113));
+	          namePanel.add(lblPrice);
+//	          mjb.addActionListener(new ActionListener()
+//	            {@Override
+//	              public void actionPerformed(ActionEvent e)
+//	                {
+//	                  jventana.remove(jventana.getVentanaActual());
+//	                  JScrollPaneCanciones jventanaCancionesDeArtista = new JScrollPaneCanciones(jventana, album, jventana.COLOR_SECUNDARIO);
+//	                  jventana.add(jventanaCancionesDeArtista, BorderLayout.CENTER);
+//	                  jventana.setVentanaActual(jventanaCancionesDeArtista);
+//	                  jventana.repaint();
+//	                  jventana.setVisible(true);
+//	                }
+//
+//	            });
+	          productPanel.add(namePanel);
+	          
+	          //Panel for the review of the product
+	          MyJPanel reviewPanel = new MyJPanel();
+	          reviewPanel.setLayout(new GridLayout(0, 1));
+	          //Panel for the stars
+	          MyJPanel starsPanel = new MyJPanel();
+	          starsPanel.add(new MyJLabel(new ImageIcon("media/icons/star.png")));
+	          starsPanel.add(new MyJLabel(new ImageIcon("media/icons/star.png")));
+	          starsPanel.add(new MyJLabel(new ImageIcon("media/icons/star.png")));
+	          starsPanel.add(new MyJLabel(new ImageIcon("media/icons/star.png")));
+	          starsPanel.add(new MyJLabel(new ImageIcon("media/icons/star.png")));
+
+	          reviewPanel.add(starsPanel);
+	          //Features of the product
+	          //Feature 1
+	          MyJLabel feature_1 = new MyJLabel("Longwear");
+	          feature_1.setForeground(Color.DARK_GRAY);
+	          reviewPanel.add(feature_1);
+	          MyJLabel feature_2 = new MyJLabel("High Coverage");
+	          feature_2.setForeground(Color.DARK_GRAY);
+	          reviewPanel.add(feature_2);
+	          MyJLabel feature_3 = new MyJLabel("Matte");
+	          feature_2.setForeground(Color.DARK_GRAY);
+	          reviewPanel.add(feature_3);
+	 
+
+	          mjb.setHorizontalAlignment(SwingConstants.CENTER);
+	        
+	          
+	          productPanel.add(reviewPanel);
+	          myJPanel_2.add(productPanel);
+
+	      }
+		
+		
+		
+	}
+	
+	public void initialiseProducts(){
+		client = new Client();
+		products = client.clientInteraction("/getProduct");
+	}
+
+}

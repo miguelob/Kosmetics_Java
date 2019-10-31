f91b6f52221adcc51914b42d50d06a60211a8630
Miguel <46598884+miguelob@users.noreply.github.com>
2019-10-31 15:34:34 +0100

    Archivos eliminados

--- prueba.java
+++
@@ -1,8 +0,0 @@
-public class prueba{
-    public static void main(String[] args) {
-        test test1 = new test();
-        test2 test21 = new test2(4);
-
-        System.out.println(test21);
-    }
-}

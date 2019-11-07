3b43e67a37e5f08b1686ecc031c46f9059b19752
Miguel <oleoblancomiguel@gmail.com>
2019-11-07 11:14:23 +0100

    ALMOST DONE!! =D

--- projectISW19/src/icai/dtc/isw/dao/TestDAO.java
+++ projectISW19/src/icai/dtc/isw/dao/TestDAO.java
@@ -1,10 +1,17 @@
 package icai.dtc.isw.dao;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
 
 import icai.dtc.isw.domain.Test;
 
@@ -12,31 +19,37 @@ public class TestDAO {
 	
 	
 	
-	public static void getTests(ArrayList<Test> lista) {
+	public static void getTests() {
+		 byte [] data = null;
+	    try {
+	    	BufferedImage bImage = ImageIO.read(new File("test.jpg"));
+	    	ByteArrayOutputStream bos = new ByteArrayOutputStream();
+			ImageIO.write(bImage, "jpg", bos );
+			data = bos.toByteArray();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		System.out.println(data);
 		Connection con=ConnectionDAO.getInstance().getConnection();
-		try (PreparedStatement pst = con.prepareStatement("SELECT * FROM test");
-                ResultSet rs = pst.executeQuery()) {
+		try{
+			PreparedStatement pst = con.prepareStatement("UPDATE \"Products\" SET \"Image\" = ? WHERE \"ID_Product\" = ?");
+			pst.setBytes(1,data);
+			pst.setInt(2, 3);
 
-            while (rs.next()) {
-            	lista.add(new Test(rs.getString(1),rs.getString(2)));
-            }
+            int row = pst.executeUpdate();
+            System.out.println(row);
 
-        } catch (SQLException ex) {
-
-            System.out.println(ex.getMessage());
+        } catch (SQLException e) {
+            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 	}
 	
 	public static void main(String[] args) {
 		
-		
-		ArrayList<Test> lista=new ArrayList<Test>();
-		TestDAO.getTests(lista);
-		
-		
-		 for (Test test : lista) {			
-			System.out.println("He leido el id: "+test.getId()+" con Texto: "+test.getName());
-		}
+		TestDAO.getTests();
 		
 	
 	}
